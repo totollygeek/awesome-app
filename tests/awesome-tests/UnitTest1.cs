95028858475d675a201878775f4f31ae9b4d7bf1
Todor Todorov <totollygeek@users.noreply.github.com>
2021-05-11 07:41:09 +0300

    initial

---
+++ tests/awesome-tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace awesome_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
